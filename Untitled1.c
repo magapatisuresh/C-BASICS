bacb13304273a31afa75544c9b688a524eb212f7
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-11-05 23:40:01 -0700

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,28 @@
+//printing all arithmetic operations:
+#include<stdio.h>
+void main()
+{
+	int a,b,s,d,p,q,m;
+	a=10;
+	b=2;
+	
+	s= a+b;
+	printf("Sum of %d and %d is %d\n",a,b,s);
+	
+	d= a-b;
+	printf("Difference of %d and %d is %d\n",a,b,d);
+	
+	p= a*b;
+	printf("Product of %d and %d is %d\n",a,b,p);
+	
+	q= a/b;
+	printf("Division of %d and %d is %d\n",a,b,q);
+	
+	m= a%b;
+	printf("Modular division of %d and %d is %d",a,b,m);
+	
+	
+	
+	
+	 
+}
