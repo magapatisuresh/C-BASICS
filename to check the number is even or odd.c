8c3de6770b5961d38c2a87ad8c7debfc7249fa3f
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-12-10 22:50:45 -0800

    Add files via upload

---
+++ to check the number is even or odd.c
@@ -0,0 +1,12 @@
+//To check the number is even or odd
+#include <stdio.h>
+void main()
+{
+	int number;
+	printf("Enter a number");
+	scanf("%d",& number);
+	if (number%2==0)
+	printf("The given number is even");
+	else
+	printf("The given number is  odd");
+}
---
+++ to check whether the number is positive or negative.c
@@ -0,0 +1,14 @@
+// To check whether the given number is positive or negative
+#include<stdio.h>
+void main()
+
+{
+		int n;
+	scanf("%d",&n);
+	if (n>=0)
+	printf("It is a positive number");
+	else
+	printf("It is a negative number");
+
+  
+  }
