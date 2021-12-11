035b9331d409f9b89c70e7d8ecd011c281ca5646
magapatisuresh <93205460+magapatisuresh@users.noreply.github.com>
2021-12-10 22:49:02 -0800

    Add files via upload

--- Untitled1.c
+++ Untitled1.c
@@ -1,28 +1,12 @@
-//printing all arithmetic operations:
+//using if condition to show person is eligible for vote or not
 #include<stdio.h>
-void main()
+void main ()
+
 {
-	int a,b,s,d,p,q,m;
-	a=10;
-	b=2;
-	
-	s= a+b;
-	printf("Sum of %d and %d is %d\n",a,b,s);
-	
-	d= a-b;
-	printf("Difference of %d and %d is %d\n",a,b,d);
-	
-	p= a*b;
-	printf("Product of %d and %d is %d\n",a,b,p);
-	
-	q= a/b;
-	printf("Division of %d and %d is %d\n",a,b,q);
-	
-	m= a%b;
-	printf("Modular division of %d and %d is %d",a,b,m);
-	
-	
-	
-	
-	 
+	int age ;
+	scanf ("%d",&age);
+	if (age>=18)
+	printf("Eligible for vote");
+	else
+	printf("Not eligible for vote");
 }
